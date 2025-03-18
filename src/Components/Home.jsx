2d2ad5534e6808e8b7161edd698a6a0b516a180f
harsh9-unofficial <justforcollegemeeting@gmail.com>
2025-03-18 11:47:42 +0530

    nth Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -61,89 +61,16 @@ const Home = () => {
 
         {/* Booking Search Component */}
         <div className="mt-14 flex justify-center px-4">
-          <div className="bg-[#F1EFE1] p-2 rounded-lg flex flex-wrap gap-2 w-full max-w-4xl justify-center">
-            {/* Check-in Field */}
-            <div className="relative flex items-center bg-white p-3 rounded-lg h-14 w-full sm:w-60 md:w-52 text-[#888888]">
-              <input
-                type="date"
-                value={checkInDate}
-                onChange={handleCheckInChange}
-                className="w-full bg-transparent focus:outline-none text-gray-700"
-                min={new Date().toISOString().split("T")[0]} // Prevent past dates
-              />
-            </div>
-
-            {/* Check-out Field */}
-            <div className="relative flex items-center bg-white p-3 rounded-lg h-14 w-full sm:w-60 md:w-52 text-[#888888]">
-              <input
-                type="date"
-                value={checkOutDate}
-                onChange={(e) => setCheckOutDate(e.target.value)}
-                className="w-full bg-transparent focus:outline-none text-gray-700"
-                min={checkInDate || new Date().toISOString().split("T")[0]} // Prevent past and invalid dates
-              />
-            </div>
-
-            {/* Guests Dropdown */}
-            <div className="relative h-14 w-full sm:w-60 md:w-52">
-              <div
-                className="flex items-center justify-between bg-white p-3 rounded-lg cursor-pointer h-full"
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              >
-                <span className="text-[#888888]">
-                  {guests.adult + guests.child} Person
-                </span>
-                <div className="flex items-center space-x-2">
-                  <FaChevronDown className="text-gray-500" />
-                  <FaUser className="text-gray-500" />
-                </div>
-              </div>
-
-              {/* Dropdown Menu */}
-              <div
-                className={`absolute left-1/2 transform -translate-x-1/2 mt-3 bg-[#F1EFE1] text-[#888888] rounded-lg shadow-lg p-2 space-y-2 w-48 transition-all duration-300 ${
-                  isDropdownOpen
-                    ? "opacity-100 scale-100"
-                    : "opacity-0 scale-95 pointer-events-none"
-                }`}
-              >
-                {["child", "adult", "room"].map((type) => (
-                  <div
-                    key={type}
-                    className="flex justify-between items-center bg-gray-100 p-2 rounded-lg"
-                  >
-                    <span className="capitalize">{type}</span>
-                    <div className="flex items-center space-x-2">
-                      <button
-                        className="p-1 bg-gray-300 rounded-full w-6 h-6 flex items-center justify-center"
-                        onClick={() => updateGuests(type, "increase")}
-                      >
-                        +
-                      </button>
-                      <span>{guests[type]}</span>
-                      <button
-                        className="p-1 bg-gray-300 rounded-full w-6 h-6 flex items-center justify-center"
-                        onClick={() => updateGuests(type, "decrease")}
-                      >
-                        -
-                      </button>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* Search Button */}
-            <button className="bg-orange-500 hover:bg-orange-600 text-white p-3 rounded-lg text-lg h-14 w-full sm:w-60 md:w-52">
-              <a
-                href="https://book-directonline.com/properties/mirambeenamotel"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Search
-              </a>
-            </button>
-          </div>
+          {/* Search Button */}
+          <button className="bg-orange-500 hover:bg-orange-600 text-white p-3 rounded-lg text-lg h-14 w-full sm:w-60 md:w-52">
+            <a
+              href="https://book-directonline.com/properties/mirambeenamotel"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Search
+            </a>
+          </button>
         </div>
       </div>
     </section>
